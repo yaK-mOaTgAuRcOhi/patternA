13b99db199e75950aa3f73471b0285286a8abfbd
KotaroYamaguchi <naturalize57@gmail.com>
2020-01-20 16:19:08 +0900

    circleci

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FootComponent } from './foot/foot.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeadComponent } from './head/head.component';
+import { DemoMaterialModule } from '../app/material-module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    DemoMaterialModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    FootComponent,
+    MenuComponent,
+    HeadComponent
+  ],
+  bootstrap: [AppComponent],
+  providers: []
 })
 export class AppModule { }
